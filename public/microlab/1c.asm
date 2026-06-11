145b3d215a819d276a917d9a302b50cb674bbeb4
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-06-11 07:51:40 +0530

    Update 1c.asm

--- public/microlab/1c.asm
+++ public/microlab/1c.asm
@@ -1,18 +1,21 @@
 ;ALP for Logical Operations
-AREA LOGICAL,CODE,READONLY
-ENTRY
+        AREA    LOGICAL, CODE, READONLY
+        ENTRY
 
-MOV R0,#0X0000
-MOV R1,#0X2040
-MOV R2,#0X007F
+        MOV     R0, #0x0000        ; R0 = 0x0000
+        MOV     R1, #0x2040        ; R1 = 0x2040
+        MOV     R2, #0x007F        ; R2 = 0x007F
 
-ORR  R0,R1,R2
-AND  R3,R1,#0X0F
-ANDS R3,R1,#0X0F
+        ORR     R0, R1, R2         ; R0 = R1 OR R2
 
-EOR R4,R1,R2
-BIC R5,R1,R2
+        AND     R3, R1, #0x0F      ; R3 = R1 AND 0x0F
 
-STOP B STOP
+        ANDS    R3, R1, #0x0F      ; R3 = R1 AND 0x0F, updates flags
 
-END
+        EOR     R4, R1, R2         ; R4 = R1 XOR R2
+
+        BIC     R5, R1, R2         ; R5 = R1 AND NOT(R2)
+
+STOP    B       STOP
+
+        END
